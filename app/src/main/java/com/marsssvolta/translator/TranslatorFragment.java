f71e1c71f8775911a0d24024435ebdbcd8920e7a
Marsssvolta <marsssvolta@gmail.com>
2019-02-05 11:38:43 +0400

    Работа над UI, 3

--- app/src/main/java/com/marsssvolta/translator/TranslatorFragment.java
+++ app/src/main/java/com/marsssvolta/translator/TranslatorFragment.java
@@ -6,13 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class TranslatorFragment extends Fragment {
+
+    private Spinner mSpinnerLanguageFrom;
+    private Spinner mSpinnerLanguageTo;
+    private ImageView mImageSwap;
+    private ImageView mClearText;
+    private EditText mTextInput;
+    private TextView mTextTranslated;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_translator, container, false);
+
+        mSpinnerLanguageFrom = view.findViewById(R.id.spinner_language_from);
+        mSpinnerLanguageTo = view.findViewById(R.id.spinner_language_to);
+        mImageSwap = view.findViewById(R.id.image_swap);
+        mClearText = view.findViewById(R.id.clear_text);
+        mTextInput = view.findViewById(R.id.text_input);
+        mTextTranslated = view.findViewById(R.id.text_translated);
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
     }
 }
